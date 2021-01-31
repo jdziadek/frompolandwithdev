eae6af97059ee98fdb5404b3e5f9b5636fdad0fe
Jakub Dziadek <j.dziadek@vasco-electronics.com>
2021-01-31 17:45:00 +0100

    add url param as default value of search input

--- src/hooks.js
+++ src/hooks.js
@@ -1,5 +1,6 @@
 import { debounce } from "lodash";
 import { useEffect, useRef, useState } from "react";
+import qs from "query-string";
 
 export const useDebouncedFunc = () => {
   const func = useRef(debounce((f) => f(), 500));
@@ -10,6 +11,14 @@ export const useSearchQuery = ({ onChangeDebouncedQuery }) => {
   const [query, setQuery] = useState("");
   const [searchQuery, setSearchQuery] = useState("");
   const debouncedSetSearchQuery = useDebouncedFunc();
+
+  useEffect(() => {
+    const parsedUrlParams = qs.parse(window.location.search);
+    if (parsedUrlParams.query) {
+      setQuery(parsedUrlParams.query);
+      setSearchQuery(parsedUrlParams.query);
+    }
+  }, []);
 
   useEffect(() => {
     debouncedSetSearchQuery(() => {
